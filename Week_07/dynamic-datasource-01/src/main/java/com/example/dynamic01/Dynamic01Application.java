692af85acf333fc9ebb031c1e89cdd83a76c75b6
yabo.zhao@yoyi.com.cn <yabo.zhao@yoyi.com.cn>
2021-03-07 03:44:02 +0800

    Week07-1

---
+++ Week_07/dynamic-datasource-01/src/main/java/com/example/dynamic01/Dynamic01Application.java
@@ -0,0 +1,13 @@
+package com.example.dynamic01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dynamic01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dynamic01Application.class, args);
+    }
+
+}
---
+++ Week_07/dynamic-datasource-01/src/test/java/com/example/dynamic01/Dynamic01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dynamic01;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dynamic01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
